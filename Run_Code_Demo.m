7d15de87bced6a2c93205b22fe91ea625a8fa95f
marcusgoff <116776757+marcusgoff@users.noreply.github.com>
2025-08-15 13:06:32 +1000

    Update Run_Code_Demo.m

--- Run_Code_Demo.m
+++ Run_Code_Demo.m
@@ -1,8 +1,24 @@
-%----------------------------------------------------%
-% Run Code Demo
-%----------------------------------------------------%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Script: code_demo.m
+% Purpose: Demo of the code used in our paper
+%          "Decoherence in Majorana Qubits by 1/f Noise"
+%
+% Author: Marcus C. Goffage
+% Date: 12-Aug-2025
+% Affiliation: University of New South Wales
+%
+% Paper: "Decoherence in Majorana Qubits by 1/f Noise"
+% Paper Authors: A. Alase^1, M. C. Goffage^2, M. C. Cassidy^2, 
+%                S. N. Coppersmith^{2*}
+% Affiliations:  ^1 University of Sydney
+%                ^2 University of New South Wales
+%                *  Corresponding Author
+%
+% -------------------------------------------------------------------------
+% ABOUT THIS SCRIPT
+% -------------------------------------------------------------------------
 % Description:
-%   - Demo code which runs in 4 minutes (tested on a Macbook pro). Calculates and plots P_qpp
+%   - Demo code which runs in under 10 minutes. Calculates and plots P_qpp
 %     as a function of time for L = 3 microns, and for a TLF rate of 200
 %     GHz, for 1 nanosecond (this corresponds to the yellow line in figure
 %     2b in the main text). 
@@ -10,13 +26,12 @@
 % Requirements:
 %   - MATLAB R2024 or newer
 %   - Dependencies: functions in /QPP_Library directory. 
-%   - Run_Figure_2c must be run before this script
 %
 % Output:
 %   - Saves Figure_2b in the ./results
 %   - Saves entire worskpace in the ./data directory
 %
-%----------------------------------------------------%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 try
