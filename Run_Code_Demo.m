e99ac859ed5e7de2fe6fc0543c3289e20c03e6c5
marcusgoff <116776757+marcusgoff@users.noreply.github.com>
2025-08-15 12:58:18 +1000

    Update and rename Code/Run_Code_Demo.m to Run_Code_Demo.m

--- Run_Code_Demo.m
+++ Run_Code_Demo.m
@@ -1,7 +1,7 @@
 %----------------------------------------------------%
 % Run Code Demo
 %----------------------------------------------------%
-
+% Expected Run Time: 4 minutes (tested on a Macbook pro)
 
 try
     run('Code/code_demo.m');
