90ee557406d7b9f542136bd1a848f59c0b557c2e
marcusgoff <116776757+marcusgoff@users.noreply.github.com>
2025-08-15 12:50:45 +1000

    Update Run_All_Figures.m

--- Code/Run_All_Figures.m
+++ Code/Run_All_Figures.m
@@ -23,18 +23,3 @@ catch ME
 end
 
 
-% try
-%     Run_Figure2b;
-%     clearvars; 
-%     Run_Figure2c;
-%     clearvars; 
-%     Run_FigureS1;
-%     clearvars; 
-%     Run_Figure3cd;
-%     clearvars; 
-%     Run_FigureS2;
-%     clearvars; 
-%     Run_FigureS4;
-% catch ME
-%     disp(getReport(ME));
-% end
