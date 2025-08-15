c1b8837365fdaad5bafeae4dd3459df8430365f1
marcusgoff <116776757+marcusgoff@users.noreply.github.com>
2025-08-15 12:50:20 +1000

    Add files via upload

--- Code/Run_All_Figures.m
+++ Code/Run_All_Figures.m
@@ -1,19 +1,40 @@
 %----------------------------------------------------%
-% Run all figures from a single instance of MATLAB
+% Run all figures 
 %----------------------------------------------------%
 
+% You may comment out any of the following lines to exclude running that
+% figure's matlab script. To comment out a line simply type a "%" character 
+% at the start of that line. 
+
 try
-    Run_Figure2b;
-    clearvars; 
-    Run_Figure2c;
-    clearvars; 
-    Run_FigureS1;
-    clearvars; 
-    Run_Figure3cd;
-    clearvars; 
-    Run_FigureS2;
-    clearvars; 
-    Run_FigureS4;
+    run('Code/Run_Figure2b.m');
+    clearvars;
+    run('Code/Run_Figure2c.m');
+    clearvars;
+    run('Code/Run_FigureS1.m');
+    clearvars;
+    run('Code/Run_Figure3cd.m');
+    clearvars;
+    run('Code/Run_FigureS2.m');
+    clearvars;
+    run('Code/Run_FigureS4.m');
 catch ME
     disp(getReport(ME));
 end
+
+
+% try
+%     Run_Figure2b;
+%     clearvars; 
+%     Run_Figure2c;
+%     clearvars; 
+%     Run_FigureS1;
+%     clearvars; 
+%     Run_Figure3cd;
+%     clearvars; 
+%     Run_FigureS2;
+%     clearvars; 
+%     Run_FigureS4;
+% catch ME
+%     disp(getReport(ME));
+% end
