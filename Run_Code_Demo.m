fa092a4fcd57748960ea37f5fa419a91c330edac
marcusgoff <116776757+marcusgoff@users.noreply.github.com>
2025-08-15 12:59:36 +1000

    Update Run_Code_Demo.m

--- Run_Code_Demo.m
+++ Run_Code_Demo.m
@@ -1,7 +1,23 @@
 %----------------------------------------------------%
 % Run Code Demo
 %----------------------------------------------------%
-% Expected Run Time: 4 minutes (tested on a Macbook pro)
+% Description:
+%   - Demo code which runs in 4 minutes (tested on a Macbook pro). Calculates and plots P_qpp
+%     as a function of time for L = 3 microns, and for a TLF rate of 200
+%     GHz, for 1 nanosecond (this corresponds to the yellow line in figure
+%     2b in the main text). 
+%
+% Requirements:
+%   - MATLAB R2024 or newer
+%   - Dependencies: functions in /QPP_Library directory. 
+%   - Run_Figure_2c must be run before this script
+%
+% Output:
+%   - Saves Figure_2b in the ./results
+%   - Saves entire worskpace in the ./data directory
+%
+%----------------------------------------------------%
+
 
 try
     run('Code/code_demo.m');
